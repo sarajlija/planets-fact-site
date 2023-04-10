39da184dc6a57bb0956dd374c00fef3f4c3606c5
Salko <64722632+sarajlija@users.noreply.github.com>
2023-04-10 15:39:59 +0200

    Update main

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import Home from "./Home"
 import Header from "./Header"
+import Earth from "./pages/PlanetEarth/Earth.js"
+import Venus from "./pages/PlanetVenus/Venus.js"
+import Jupiter from "./pages/PlanetJupiter/Jupiter.js"
+import Neptune from "./pages/PlanetNeptune/Neptune.js"
+import Uranus from "./pages/PlanetUranus/Uranus.js"
+import Saturn from "./pages/PlanetSaturn/Saturn"
+import Mars from "./pages/PlanetMar/Mars"
 
 function App() {
   return (
     <main>
       <Header />
+      <hr className="text-light my-0 " />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/earth" element={<Earth />} />
+        <Route path="/venus" element={<Venus />} />
+        <Route path="/mars" element={<Mars />} />
+        <Route path="/jupiter" element={<Jupiter />} />
+        <Route path="/saturn" element={<Saturn />} />
+        <Route path="/uranus" element={<Uranus />} />
+        <Route path="/neptune" element={<Neptune />} />
       </Routes>
     </main>
   )
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,31 @@
 import React from "react"
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
 
 function Header() {
-  return <div>Header</div>
+  return (
+    <Navbar bg="" expand="sm">
+      <Container fluid>
+        <Navbar.Brand href="#" className="text-light">
+          The planets
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="ms-auto my-2 my-lg-0 " style={{ maxHeight: "85px" }} navbarScroll>
+            <Nav.Link href="#action1">Mercury</Nav.Link>
+            <Nav.Link href="#action2">Venus</Nav.Link>
+            <Nav.Link href="#action3">Earth</Nav.Link>
+            <Nav.Link href="#action4">Mars</Nav.Link>
+            <Nav.Link href="#action5">Jupiter</Nav.Link>
+            <Nav.Link href="#action6">Saturn</Nav.Link>
+            <Nav.Link href="#action7">Uranus</Nav.Link>
+            <Nav.Link href="#action8">Neptune</Nav.Link>
+          </Nav>{" "}
+        </Navbar.Collapse>
+      </Container>{" "}
+    </Navbar>
+  )
 }
 
 export default Header
