0366045dfa17d5827b1da6e71668c4be85a69069
Salko <64722632+sarajlija@users.noreply.github.com>
2023-04-10 22:16:25 +0200

    update

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from "react"
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
+import data from "./data.json"
 
 function Header() {
   return (
@@ -13,17 +14,22 @@ function Header() {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="ms-auto my-2 my-lg-0 " style={{ maxHeight: "85px" }} navbarScroll>
-            <Nav.Link href="#action1">Mercury</Nav.Link>
+            {data.map((item, index) => (
+              <Nav.Link key={index} href={`#active ${index}`}>
+                {item.name}
+              </Nav.Link>
+            ))}
+            {/*    
             <Nav.Link href="#action2">Venus</Nav.Link>
             <Nav.Link href="#action3">Earth</Nav.Link>
             <Nav.Link href="#action4">Mars</Nav.Link>
             <Nav.Link href="#action5">Jupiter</Nav.Link>
             <Nav.Link href="#action6">Saturn</Nav.Link>
             <Nav.Link href="#action7">Uranus</Nav.Link>
-            <Nav.Link href="#action8">Neptune</Nav.Link>
-          </Nav>{" "}
+            <Nav.Link href="#action8">Neptune</Nav.Link>*/}
+          </Nav>
         </Navbar.Collapse>
-      </Container>{" "}
+      </Container>
     </Navbar>
   )
 }
